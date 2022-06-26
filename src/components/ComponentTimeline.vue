fc7aff8806127ab8bf413015906d8dcf735eb5bb
TechBond7 <techbond7@outlook.com>
2022-06-26 11:17:14 +0200

    added new experiences

--- src/components/ComponentTimeline.vue
+++ src/components/ComponentTimeline.vue
@@ -54,7 +54,7 @@ export default defineComponent({
         icon: "pi pi-desktop",
         color: color.value,
         skills:
-          "Algorithms, Data Structures, Problem Solving, .NET 6, Entity Framework (Model/Code First), C#10, Xamarin.Forms, .NET MAUI?, Swagger UI, xUnit, MVC, MVVM, DDD, Java JDK 11+, Spring Boot 2+, Hibernate, QueryDSL, HATEOAS, JUnit, Mockito, Gradle, Maven, RESTful API, Javascript ES6, React 18+, Vue 3 (Options API/Composition API), PrimeVUE, Animate CSS, Router, Store, YAML, OAuth2, OpenID, JWT, Docker, Heroku, Netlify, CD, MSSQL, MySQL, DBeaver, Microsoft SQL Management Studio 2019, Visual Studio 2022, Visual Studio Code, Insomnia, Git, GitHub, Azure DevOps, Azure Active Directory, Windows Terminal, Powershell Core...",
+          "Algorithms, Data Structures, Problem Solving, .NET 6, Entity Framework (Model/Code First), C#10, Xamarin.Forms, .NET MAUI?, Swagger UI, xUnit, MVC, MVVM, DDD, Java JDK 11+, Spring Boot 2+, Hibernate, QueryDSL, HATEOAS, JUnit, Mockito, Gradle, Maven, RESTful API, Javascript ES6, React 18+, Vue 3 (Options API/Composition API), PrimeVUE, Animate CSS, Router, Store, YAML, OAuth2, OpenID, JWT, Docker, Heroku, Netlify, CD, MSSQL, MySQL, DBeaver, Microsoft SQL Management Studio 2019, Transact-SQL, Visual Studio 2022, Visual Studio Code, Insomnia, Git, GitHub, Azure DevOps, Azure Active Directory, Windows Terminal, Powershell Core...",
         activity: "Software development",
       },
       {
@@ -64,7 +64,7 @@ export default defineComponent({
         icon: "pi pi-desktop",
         color: color.value,
         skills:
-          ".NET 6, Entity Framework (Model/Code First), C#10, Xamarin.Forms, .NET MAUI?, Swagger UI, React 18, Microsoft Dynamics 365, Power Platform, Git, Azure DevOps, Azure Active Directory, Windows Terminal, PowerShell Core, Microsoft SQL Management Studio 2019, Visual Studio 2022, Visual Studio Code, MVC, MVVM...",
+          ".NET 6, Entity Framework (Model/Code First), C#10, Xamarin.Forms, .NET MAUI?, Swagger UI, React 18, Microsoft Dynamics 365, Power Platform, Git, Azure DevOps, Azure Active Directory, Windows Terminal, PowerShell Core, Microsoft SQL Management Studio 2019, Transact-SQL, Visual Studio 2022, Visual Studio Code, MVC, MVVM...",
         activity: "Software development",
       },
       {
