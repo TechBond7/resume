76af4a11aad738b69df916b71114c3acbf51ebe7
TechBond7 <techbond7@outlook.com>
2022-06-30 23:56:40 +0200

    Fixed date values comparison

--- src/services/github.js
+++ src/services/github.js
@@ -12,7 +12,7 @@ const commitsUrl = `https://api.github.com/repos/mezdelex/replaceMe/commits`;
 
 export const api = {
     "getUpdatedRepo": () => fetch(reposUrl).then(response => response.json()).then(data => {
-        store.repo = data.reduce((previous, next) => new Date(previous.updated_at).toISOString() > new Date(next.updated_at).toISOString() ? previous.name : next.name, "");
+        store.repo = data.reduce((previous, next) => previous.updated_at.substring(0, 10) > next.updated_at.substring(0, 10) ? previous.name : next.name, "");
     }).catch(error => console.log(error)),
     "getLastCommit": () => {
         fetch(commitsUrl.replace("replaceMe", store.repo)).then(response => response.json()).then(data => {
